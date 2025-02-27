2109fcff5e1a68d68b10e00cf00b031affbd1f02
Bhavaneetharan Loganathan <bhavanl@techprovint.com>
2025-02-27 18:40:55 +0530

    updates

--- my_agent/utils/tools.py
+++ my_agent/utils/tools.py
@@ -1,3 +1,21 @@
-from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import AIMessage
+from langchain_core.tools import tool
 
-tools = [TavilySearchResults(max_results=1)]
+
+@tool
+def search_web(query: str):
+    "Call to search the web with any query"
+    print("Searching the web for:", query)
+    results = "This is a test of search_web"
+    return results
+
+
+@tool
+def calculate_math(expression: str):
+    "Call to calculate math with any expression"
+    print("Calculating math for:", expression)
+    results = "This is a test of calculate_math"
+    return results
+
+
+tools = [search_web, calculate_math]
